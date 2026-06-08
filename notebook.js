093619e18bdbb109a3825fba6889344fa33a058f
David <6024166+os9sur@users.noreply.github.com>
2026-06-08 23:07:29 +0800

    doc:  update README

--- notebook.js
+++ notebook.js
@@ -1,6 +1,6 @@
 /**
  * Mira Translator
- * Copyright (C) 2026 David Bai (Mira Studio)
+ * Copyright (C) 2026 David Bai 
  * License: AGPL-3.0 (https://github.com/os9sur)
  * Contact: mira.studio@proton.me
  */
